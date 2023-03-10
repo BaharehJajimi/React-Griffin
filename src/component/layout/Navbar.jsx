13e5108b5874731a00c703cbb31c954294aed641
BaharehJajimi <jajimi.bahareh@gmail.com>
2023-03-10 23:01:21 +0330

    Nav

--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -1,110 +1,74 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  const [show, setShow] = useState(false);
+const Navbar = () => {
   return (
-    <div className="sticky top-0 z-50">
-      <div className="w-90 h-20 flex justify-between rounded-xl rounded-t-none border border-white/80 bg-white bg-opacity-80 py-2 px-4 text-white drop-shadow-md backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-        <div className="w-full  h-full flex justify-end items-center">
-          <nav className="w-full">
-            <div className="py-5 md:py-0 container mx-auto px-6 flex items-center justify-between">
-              <div aria-label="Home. logo" role="img">
-                <img
-                  className="w-12 md:w-auto"
-                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/centre_aligned_simple-Svg1.svg"
-                  alt="logo"
-                />
-              </div>
-
-              <div>
-                <button
-                  onClick={() => setShow(!show)}
-                  className={`${
-                    show ? "hidden" : ""
-                  } sm:block md:hidden text-gray-500 hover:text-gray-700 focus:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500`}
-                >
-                  <svg
-                    aria-haspopup="true"
-                    aria-label="open Main Menu"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="md:hidden icon icon-tabler icon-tabler-menu"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#2c3e50"
-                    fill="none"
-                    strokeLinecap="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" />
-                    <line x1={4} y1={8} x2={20} y2={8} />
-                    <line x1={4} y1={16} x2={20} y2={16} />
-                  </svg>
-                </button>
-                <div
-                  id="menu"
-                  className={` ${show ? "" : "hidden"} md:block lg:block `}
-                >
-                  <button
-                    onClick={() => setShow(!show)}
-                    className={`block md:hidden lg:hidden text-gray-500 hover:text-gray-700 focus:text-gray-700 fixed focus:outline-none focus:ring-2 focus:ring-gray-500 z-30 top-0 mt-6`}
-                  >
-                    <svg
-                      aria-label="close main menu"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="#2c3e50"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" />
-                      <line x1={18} y1={6} x2={6} y2={18} />
-                      <line x1={6} y1={6} x2={18} y2={18} />
-                    </svg>
-                  </button>
-                  <ul className="flex text-3xl md:text-base items-center py-10 md:flex flex-col md:flex-row justify-center fixed md:relative top-0 bottom-0 left-0 right-0 bg-white md:bg-transparent z-20">
-                    <li className="cursor-pointer">
-                      <Link className="text-blue-900 hover:text-blue-500 text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10" to="/">
-                        بحر
-                      </Link>
-                    </li>
-                    <li className="cursor-pointer">
-                      <Link className="text-blue-900 hover:text-blue-500 text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10" to="/courses">
-                        دوره های آموزشی
-                      </Link>
-                    </li>
-                    <li className="cursor-pointer">
-                      <Link className="text-blue-900 hover:text-blue-500 text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10" to="/articles/:page">
-                        اخبار و مقالات
-                      </Link>
-                    </li>
-                    <li className="cursor-pointer">
-                      <Link className="text-blue-900 hover:text-blue-500 text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10" to="javascript: void(0)">
-                        مهارت های نرم
-                      </Link>
-                    </li>
-                    <li className="cursor-pointer">
-                      <Link className="text-blue-900 hover:text-blue-500 text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10" to="/about">
-                        درباره ما
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <Link to="/login">
-                <button className="lg:font-bold lg:text-xl shadow-md bg-orange-500 hidden md:block transition duration-150 ease-in-out border border-orange-400 hover:bg-orange-200 rounded-lg hover:border-orange-400 text-blue-900 px-4 sm:px-8 py-1 sm:py-3 sm:text-sm">
-                  ورود / عضویت
-                </button>
-              </Link>
-            </div>
-          </nav>
-        </div>
+    <header class="sticky top-0 z-50">
+      <div class="mx-auto flex-wrap p-4 flex-col md:flex-row items-center flex rounded-xl rounded-t-none border border-white/80 bg-white bg-opacity-80 drop-shadow-md backdrop-blur-2xl backdrop-saturate-200">
+        <a class="flex title-font font-medium items-center text-blue-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+        </a>
+        <nav class="mx-auto flex flex-wrap items-center lg:text-xl lg:font-medium justify-center">
+          <a class="mr-5 hover:text-gray-900">
+            <Link
+              className="text-blue-900 hover:text-blue-500 lg:pl-4"
+              to="/"
+            >
+              بحر
+            </Link>
+          </a>
+          <a class="mr-4 hover:text-gray-900">
+            <Link
+              className="text-blue-900 hover:text-blue-500 lg:pl-4"
+              to="/courses"
+            >
+              دوره های آموزشی
+            </Link>
+          </a>
+          <a class="mr-4 hover:text-gray-900">
+            <Link
+              className="text-blue-900 hover:text-blue-500 lg:pl-4"
+              to="/articles/:page"
+            >
+              اخبار و مقالات
+            </Link>
+          </a>
+          <a class="mr-4 hover:text-gray-900">
+            <Link
+              className="text-blue-900 hover:text-blue-500 lg:pl-4"
+              to="javascript: void(0)"
+            >
+              دسته بندی  
+            </Link>
+          </a>
+          <a class="mr-4 hover:text-gray-900">
+            <Link
+              className="text-blue-900 hover:text-blue-500 lg:pl-4"
+              to="/about"
+            >
+              درباره ما
+            </Link>
+          </a>
+        </nav>
+        <Link to="/login">
+          <button className="lg:font-bold lg:text-xl lg:px-4 lg:py-3 lg:mt-0 px-2 py-1 mt-5 shadow-md bg-orange-500 transition duration-150 ease-in-out border border-orange-400 hover:bg-orange-200 rounded-lg hover:border-orange-400 text-blue-900">
+            ورود / عضویت
+          </button>
+        </Link>
       </div>
-    </div>
+    </header>
   );
-}
+};
+
+export default Navbar;
